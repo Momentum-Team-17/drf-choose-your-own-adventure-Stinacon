007b865b0f29bbe1bd3cf9619b98801740f9ddef
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-21 16:46:36 -0400

    create book and author with foreign key relationship

--- library/models.py
+++ library/models.py
@@ -4,3 +4,24 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Book(models.Model):
+    CHOICES = (
+        ('self-help', 'Self-Help'),
+        ('graphic', 'Graphic Novel'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('comedy', 'Comedy'),
+        ('spy', 'Spy'),
+    )
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        to='Author', on_delete=models.CASCADE, related_name='books_by_author')
+    date_published = models.DateField(blank=True, null=True)
+    genre = models.CharField(choices=CHOICES, max_length=50)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
