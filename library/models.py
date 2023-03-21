2dfea84886adf2171fbe8d4185034872c53aca91
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-21 17:19:37 -0400

    added unique constraint to book model re: title and author

--- library/models.py
+++ library/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.constraints import UniqueConstraint
 
 
 class User(AbstractUser):
     pass
 
 
+# add string methods for book and author models
 class Book(models.Model):
     CHOICES = (
         ('self-help', 'Self-Help'),
@@ -18,10 +20,22 @@ class Book(models.Model):
 
     title = models.CharField(max_length=100)
     author = models.ForeignKey(
-        to='Author', on_delete=models.CASCADE, related_name='books_by_author')
+        to='Author', on_delete=models.CASCADE)
     date_published = models.DateField(blank=True, null=True)
     genre = models.CharField(choices=CHOICES, max_length=50)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['title', 'author'], name='constraints')
+        ]
+
+    def __str__(self):
+        return self.title
 
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
